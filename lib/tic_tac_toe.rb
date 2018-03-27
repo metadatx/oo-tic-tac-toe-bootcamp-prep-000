54d9622163ef16980fae963e4c051e82f9b62a1d
Firat Aksu <aksu_fr@yahoo.es>
2018-03-27 11:50:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,7 +17,7 @@ class TicTacToe
     return input.to_i - 1
   end
 
-  def move(board, current_player)
+  def move(current_player)
     return @board[index] = current_player
   end
 
@@ -28,11 +28,74 @@ class TicTacToe
   def valid_move?(index)
     return index.between?(0,8) && !position_taken?(index)
   end
-  
+
   def turn_count
-    @board.count do |value| 
-      value != " " 
+    @board.count do |value|
+      value != " "
     end
   end
-  
+
+  def current_player
+    return (turn_count % 2 == 0) ? "X" : "O"
+  end
+
+  def turn
+   puts "Please enter a number (1-9):"
+   input = gets.strip
+   index = input_to_index(input)
+   if valid_move?(index)
+     move(index, current_player())
+   else
+     turn()
+   end
+   display_board()
+ end
+
+ def won?
+    WIN_COMBINATIONS.each do |win_condition|
+      if ((@board[win_condition[0]] == "X" && @board[win_condition[1]] == "X" && @board[win_condition[2]] == "X") ||
+          (@board[win_condition[0]] == "O" && @board[win_condition[1]] == "O" && @board[win_condition[2]] == "O"))
+          return win_condition
+      end
+    end
+    return false
+  end
+
+  def full?
+    @board.each do |value|
+      if value != "X" && value != "O"
+        return false
+      end
+    end
+    return true
+  end
+
+  def draw?
+      if (!won? && full?)
+        return true
+  end
+
+  def over?
+      if (won? || draw?)
+        return true
+      else
+        return false
+      end
+    end
+
+  def winner
+    if(win_cond = won?)
+      return @board[win_cond[0]]
+    end
+      return nil
+  end
+
+  def play
+    if over?
+      if draw? puts "draw"
+      else puts "winner is" + winner()
+      end
+    end
+  end
+
 end
